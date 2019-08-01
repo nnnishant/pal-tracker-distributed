1eac06456d41420f026fc96f1f034e71031c6da8
kumaragurusankar <kumaraguru.marees@gmail.com>
2019-08-01 16:00:09 -0400

    OAuth Added

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestOperations;
 
 import java.util.TimeZone;
@@ -16,6 +19,9 @@ import java.util.TimeZone;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableWebSecurity
+@EnableResourceServer
+@EnableOAuth2Client
 @ComponentScan({"io.pivotal.pal.tracker.backlog", "io.pivotal.pal.tracker.restsupport"})
 public class App {
 
